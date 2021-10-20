0273564d912fa590727bc9d11a647f7710907f0e
Richard Bradley <hirdrac@users.noreply.github.com>
2021-10-20 21:56:19 +0900

    added formatInt()

--- gx/StringUtil.hh
+++ gx/StringUtil.hh
@@ -4,7 +4,11 @@
 //
 
 #pragma once
+#include <string>
 #include <sstream>
+#include <limits>
+#include <type_traits>
+#include <cstdint>
 
 
 namespace gx
@@ -15,5 +19,36 @@ namespace gx
     std::ostringstream os;
     ((os << args),...);
     return os.str();
+  }
+
+  template<typename IntType>
+  [[nodiscard]] std::string formatInt(IntType n, char separator = ',')
+  {
+    static_assert(std::is_integral_v<IntType>);
+    if (n == 0) { return "0"; }
+
+    uint64_t v;
+    static_assert(sizeof(IntType) <= sizeof(v));
+
+    if constexpr (std::is_unsigned_v<IntType>) {
+      v = n;
+    } else if (n == std::numeric_limits<IntType>::min()) {
+      v = uint64_t{std::numeric_limits<IntType>::max()} + 1;
+    } else {
+      v = (n<0) ? -n : n;
+    }
+
+    char tmp[32];
+    char* ptr = &tmp[sizeof(tmp) - 1];
+    *ptr = '\0';
+
+    int i = 0;
+    for (; v > 0; v /= 10) {
+      *(--ptr) = '0' + (v % 10);
+      if (v >= 10 && (++i % 3) == 0) { *(--ptr) = separator; }
+    }
+
+    if (n < 0) { *(--ptr) = '-'; }
+    return ptr;
   }
 }
