a84a6de0474b91e23302fe89c7501efdd1133622
Richard Bradley <hirdrac@users.noreply.github.com>
2021-12-09 04:03:53 +0900

    cleanup

--- gx/Font.cc
+++ gx/Font.cc
@@ -5,7 +5,8 @@
 
 // TODO: investigate using stb_truetype.h
 // TODO: use signed distance fields for font texture
-//       (FT_RENDER_MODE_SDF available in freetype 2.11)
+//   - FT_RENDER_MODE_SDF available in freetype 2.11
+//   - multi-threaded rendering requires one FT_Library instance per thread
 
 #include "Font.hh"
 #include "Image.hh"
@@ -14,71 +15,70 @@
 #include <cstdlib>
 #include <cstring>
 #include <cassert>
-
 #include <ft2build.h>
 #include <freetype/freetype.h>
 using namespace gx;
 
 
-namespace {
-  FT_Library ftLib = nullptr;
+static FT_Library ftLib = nullptr;
 
-  void cleanupFreeType()
-  {
-    if (ftLib && FT_Done_FreeType(ftLib)) {
-      GX_LOG_ERROR("FT_Done_FreeType() failed");
-    }
-
-    ftLib = nullptr;
+static void cleanupFreeType()
+{
+  if (ftLib && FT_Done_FreeType(ftLib)) {
+    GX_LOG_ERROR("FT_Done_FreeType() failed");
   }
 
-  bool initFreeType()
-  {
-    if (ftLib) { return true; }
+  ftLib = nullptr;
+}
 
-    if (FT_Init_FreeType(&ftLib)) {
-      GX_LOG_ERROR("FT_Init_FreeType() failed");
+static bool initFreeType()
+{
+  if (ftLib) { return true; }
+
+  if (FT_Init_FreeType(&ftLib)) {
+    GX_LOG_ERROR("FT_Init_FreeType() failed");
+    return false;
+  }
+
+  std::atexit(cleanupFreeType);
+  return true;
+}
+
+static bool genGlyphs(Font& font, FT_Face face,
+                      uint32_t start = 0, uint32_t end = 0)
+{
+  // read font glyph data
+  const bool hasVert = FT_HAS_VERTICAL(face);
+  FT_UInt index = 0;
+  FT_ULong ch = FT_Get_First_Char(face, &index);
+
+  for (; index != 0; ch = FT_Get_Next_Char(face, ch, &index)) {
+    if (ch < 32 || ch < start) { continue; }
+    else if (end != 0 && ch > end) { break; }
+
+    if (FT_Load_Glyph(face, index, FT_LOAD_DEFAULT)) {
+      GX_LOG_ERROR("FT_Load_Glyph() failed");
       return false;
     }
 
-    std::atexit(cleanupFreeType);
-    return true;
-  }
-
-  bool genGlyphs(Font& font, FT_Face face,
-                 uint32_t start = 0, uint32_t end = 0)
-  {
-    // read font glyph data
-    const bool hasVert = FT_HAS_VERTICAL(face);
-    FT_UInt index = 0;
-    FT_ULong ch = FT_Get_First_Char(face, &index);
-
-    for (; index != 0; ch = FT_Get_Next_Char(face, ch, &index)) {
-      if (ch < 32 || ch < start) { continue; }
-      else if (end != 0 && ch > end) { break; }
-
-      if (FT_Load_Glyph(face, index, FT_LOAD_DEFAULT)) {
-        GX_LOG_ERROR("FT_Load_Glyph() failed");
-        return false;
-      }
-
-      FT_GlyphSlot gs = face->glyph;
-      if (FT_Render_Glyph(gs, FT_RENDER_MODE_NORMAL)) {
-        GX_LOG_ERROR("FT_Render_Glyph() failed");
-        return false;
-      }
-
-      const float advX = float(gs->advance.x) / 64.0f;
-      const float advY = hasVert ? (float(gs->advance.y) / 64.0f) : 0;
-      font.addGlyph(int(ch), int(gs->bitmap.width), int(gs->bitmap.rows),
-                    float(gs->bitmap_left), float(gs->bitmap_top),
-                    advX, advY, gs->bitmap.buffer, true);
+    FT_GlyphSlot gs = face->glyph;
+    if (FT_Render_Glyph(gs, FT_RENDER_MODE_NORMAL)) {
+      GX_LOG_ERROR("FT_Render_Glyph() failed");
+      return false;
     }
-    return true;
+
+    const float advX = float(gs->advance.x) / 64.0f;
+    const float advY = hasVert ? (float(gs->advance.y) / 64.0f) : 0;
+    font.addGlyph(int(ch), int(gs->bitmap.width), int(gs->bitmap.rows),
+                  float(gs->bitmap_left), float(gs->bitmap_top),
+                  advX, advY, gs->bitmap.buffer, true);
   }
+
+  return true;
 }
 
 
+// **** Font class ****
 bool Font::load(const char* fileName, int fontSize)
 {
   if (!initFreeType()) { return false; }
