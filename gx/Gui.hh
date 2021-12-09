089c5c5618ceda2aa9534940648795cbf2bb1f59
Richard Bradley <hirdrac@users.noreply.github.com>
2021-12-10 03:03:42 +0900

    moved GuiTheme into its own file

--- gx/Gui.hh
+++ gx/Gui.hh
@@ -10,10 +10,11 @@
 // TODO: list select (combo box)
 
 #pragma once
+#include "GuiTheme.hh"
 #include "DrawList.hh"
 #include "Align.hh"
-#include "Color.hh"
 #include "Types.hh"
+#include <string>
 #include <string_view>
 #include <vector>
 #include <initializer_list>
@@ -30,7 +31,6 @@ namespace gx {
   class Font;
   class Gui;
   struct GuiElem;
-  struct GuiTheme;
 
   enum GuiElemType {
     GUI_NULL = 0,
@@ -98,74 +98,6 @@ struct gx::GuiElem
     : type{t}, align{a}, id{i} { }
   GuiElem(GuiElemType t, AlignEnum a, int i, std::initializer_list<GuiElem> x)
     : elems{x}, type{t}, align{a}, id{i} { }
-};
-
-
-struct gx::GuiTheme
-{
-  struct Style {
-    RGBA8 textColor;
-    RGBA8 backgroundColor;
-    RGBA8 edgeColor;
-  };
-
-  const Font* font = nullptr;
-  Style panel = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.2f,.2f,.2f,1.0f), 0};
-
-  Style button = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.4f,.4f,.4f,1.0f), 0};
-  Style buttonHover = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.8f,.4f,.4f,1.0f), 0};
-  Style buttonPress = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.8f,.8f,.8f,1.0f), 0};
-  Style buttonHold = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.6f,.6f,.6f,1.0f), 0};
-
-  Style checkbox = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.4f,.4f,.4f,1.0f), 0};
-  Style checkboxHover = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.8f,.4f,.4f,1.0f), 0};
-  Style checkboxPress = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.8f,.8f,.8f,1.0f), 0};
-  Style checkboxHold = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.6f,.6f,.6f,1.0f), 0};
-
-  Style menuButton = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), 0, 0};
-  Style menuButtonHover = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.8f,.4f,.4f,1.0f), 0};
-  Style menuButtonOpen = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.6f,.6f,.6f,1.0f), 0};
-  Style menuFrame = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(0.0f,0.0f,0.0f,1.0f), 0};
-  Style menuItemSelect = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.8f,.8f,.8f,1.0f), 0};
-
-  Style entry = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(0.0f,0.0f,.2f,1.0f), 0};
-  Style entryFocus = {
-    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.1f,.1f,.3f,1.0f), 0};
-
-  uint16_t entryLeftMargin = 4;
-  uint16_t entryRightMargin = 4;
-  uint16_t entryTopMargin = 2;
-  uint16_t entryBottomMargin = 2;
-
-  int32_t checkCode = 'X';
-  int16_t checkXOffset = 0;
-  int16_t checkYOffset = 2;
-
-  int32_t passwordCode = 8226; // U+2022 (bullet)
-
-  RGBA8 cursorColor = packRGBA8(1.0f,1.0f,.6f,1.0f);
-  uint32_t cursorBlinkTime = 400000; // 1 sec
-  uint16_t cursorWidth = 3;
-
-  uint16_t border = 6;
-  uint16_t frameSpacing = 4;
-  uint16_t textSpacing = 1;
-  uint16_t lineWidth = 2;
 };
 
 
---
+++ gx/GuiTheme.hh
@@ -0,0 +1,82 @@
+//
+// gx/GuiTheme.hh
+// Copyright (C) 2021 Richard Bradley
+//
+
+#pragma once
+#include "Color.hh"
+#include "Types.hh"
+
+
+namespace gx {
+  class Font;
+  struct GuiTheme;
+}
+
+
+struct gx::GuiTheme
+{
+  struct Style {
+    RGBA8 textColor;
+    RGBA8 backgroundColor;
+    RGBA8 edgeColor;
+  };
+
+  const Font* font = nullptr;
+  Style panel = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.2f,.2f,.2f,1.0f), 0};
+
+  Style button = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.4f,.4f,.4f,1.0f), 0};
+  Style buttonHover = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.8f,.4f,.4f,1.0f), 0};
+  Style buttonPress = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.8f,.8f,.8f,1.0f), 0};
+  Style buttonHold = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.6f,.6f,.6f,1.0f), 0};
+
+  Style checkbox = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.4f,.4f,.4f,1.0f), 0};
+  Style checkboxHover = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.8f,.4f,.4f,1.0f), 0};
+  Style checkboxPress = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.8f,.8f,.8f,1.0f), 0};
+  Style checkboxHold = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.6f,.6f,.6f,1.0f), 0};
+
+  Style menuButton = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), 0, 0};
+  Style menuButtonHover = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.8f,.4f,.4f,1.0f), 0};
+  Style menuButtonOpen = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.6f,.6f,.6f,1.0f), 0};
+  Style menuFrame = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(0.0f,0.0f,0.0f,1.0f), 0};
+  Style menuItemSelect = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.8f,.8f,.8f,1.0f), 0};
+
+  Style entry = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(0.0f,0.0f,.2f,1.0f), 0};
+  Style entryFocus = {
+    packRGBA8(1.0f,1.0f,1.0f,1.0f), packRGBA8(.1f,.1f,.3f,1.0f), 0};
+
+  uint16_t entryLeftMargin = 4;
+  uint16_t entryRightMargin = 4;
+  uint16_t entryTopMargin = 2;
+  uint16_t entryBottomMargin = 2;
+
+  int32_t checkCode = 'X';
+  int16_t checkXOffset = 0;
+  int16_t checkYOffset = 2;
+
+  int32_t passwordCode = 8226; // U+2022 (bullet)
+
+  RGBA8 cursorColor = packRGBA8(1.0f,1.0f,.6f,1.0f);
+  uint32_t cursorBlinkTime = 400000; // 1 sec
+  uint16_t cursorWidth = 3;
+
+  uint16_t border = 6;
+  uint16_t frameSpacing = 4;
+  uint16_t textSpacing = 1;
+  uint16_t lineWidth = 2;
+};
