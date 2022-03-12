b55bda64abc7c656d3dba5544d9f22d5e9347413
Richard Bradley <hirdrac@users.noreply.github.com>
2022-03-13 05:14:41 +0900

    comment update

--- gx/LIB_gx.mk
+++ gx/LIB_gx.mk
@@ -6,5 +6,6 @@ LIB_gx.SRC =\
   Texture.cc Camera.cc Gui.cc Renderer.cc OpenGLRenderer.cc OpenGL.cc\
   3rd/glad.c 3rd/stb_image.c
 LIB_gx.PACKAGES = glfw3:3.2 freetype2
+# freetype2:24 for SDF support
 LIB_gx.TYPE = static
 LIB_gx.STANDARD = c++17
