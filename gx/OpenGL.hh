203d495c76e16386f6fc4caa991153e7de04a68a
Richard Bradley <hirdrac@users.noreply.github.com>
2022-04-09 02:56:31 +0900

    added GLBaseFormat() utility function

--- gx/OpenGL.hh
+++ gx/OpenGL.hh
@@ -67,17 +67,6 @@ int GLCheckErrors(std::string_view msg, const char* file = __FILE__,
 [[nodiscard]] constexpr GLboolean GLBool(bool val) {
   return val ? GL_TRUE : GL_FALSE; }
 
-[[nodiscard]] constexpr GLenum GLFormat(int channels)
-{
-  switch (channels) {
-    case 1:  return GL_RED;
-    case 2:  return GL_RG;
-    case 3:  return GL_RGB;
-    case 4:  return GL_RGBA;
-    default: return GL_NONE;
-  }
-}
-
 [[nodiscard]] constexpr int GLTypeSize(GLenum type)
 {
   switch (type) {
@@ -136,10 +125,129 @@ int GLCheckErrors(std::string_view msg, const char* file = __FILE__,
         s *= 4; break;
 
       default:
+        // GL_RED, GL_DEPTH_COMPONENT, GL_DEPTH_STENCIL, GL_STENCIL_INDEX
         break;
     }
   }
   return s;
+}
+
+[[nodiscard]] constexpr GLenum GLFormat(int channels)
+{
+  switch (channels) {
+    case 1:  return GL_RED;
+    case 2:  return GL_RG;
+    case 3:  return GL_RGB;
+    case 4:  return GL_RGBA;
+    default: return GL_NONE;
+  }
+}
+
+[[nodiscard]] constexpr GLenum GLBaseFormat(GLenum internalformat)
+{
+  switch (internalformat) {
+    case GL_DEPTH_COMPONENT:
+    case GL_DEPTH_COMPONENT16:
+    case GL_DEPTH_COMPONENT24:
+    case GL_DEPTH_COMPONENT32:
+    case GL_DEPTH_COMPONENT32F:
+      return GL_DEPTH_COMPONENT;
+
+    case GL_DEPTH_STENCIL:
+    case GL_DEPTH24_STENCIL8:
+    case GL_DEPTH32F_STENCIL8:
+      return GL_DEPTH_STENCIL;
+
+    case GL_STENCIL_INDEX:
+    case GL_STENCIL_INDEX1:
+    case GL_STENCIL_INDEX4:
+    case GL_STENCIL_INDEX8:
+    case GL_STENCIL_INDEX16:
+      return GL_STENCIL_INDEX;
+
+    case GL_RED:
+    case GL_R8:       case GL_R8_SNORM:
+    case GL_R8I:      case GL_R8UI:
+    case GL_R16:      case GL_R16_SNORM:
+    case GL_R16I:     case GL_R16UI:
+    case GL_R16F:
+    case GL_R32I:     case GL_R32UI:
+    case GL_R32F:
+    case GL_COMPRESSED_RED:
+    case GL_COMPRESSED_RED_RGTC1:
+    case GL_COMPRESSED_SIGNED_RED_RGTC1:
+    case GL_COMPRESSED_R11_EAC:
+    case GL_COMPRESSED_SIGNED_R11_EAC:
+      return GL_RED;
+
+    case GL_RG:
+    case GL_RG8:      case GL_RG8_SNORM:
+    case GL_RG8I:     case GL_RG8UI:
+    case GL_RG16:     case GL_RG16_SNORM:
+    case GL_RG16I:    case GL_RG16UI:
+    case GL_RG16F:
+    case GL_RG32I:    case GL_RG32UI:
+    case GL_RG32F:
+    case GL_COMPRESSED_RG:
+    case GL_COMPRESSED_RG_RGTC2:
+    case GL_COMPRESSED_SIGNED_RG_RGTC2:
+    case GL_COMPRESSED_RG11_EAC:
+    case GL_COMPRESSED_SIGNED_RG11_EAC:
+      return GL_RG;
+
+    case GL_RGB:
+    case GL_R3_G3_B2:
+    case GL_RGB4:
+    case GL_RGB5:
+    case GL_RGB8:     case GL_RGB8_SNORM:
+    case GL_RGB8I:    case GL_RGB8UI:
+    case GL_RGB10:
+    case GL_RGB12:
+    case GL_RGB16:    case GL_RGB16_SNORM:
+    case GL_RGB16I:   case GL_RGB16UI:
+    case GL_RGB16F:
+    case GL_RGB32I:   case GL_RGB32UI:
+    case GL_RGB32F:
+    case GL_R11F_G11F_B10F:
+    case GL_RGB9_E5:
+    case GL_SRGB:
+    case GL_SRGB8:
+    case GL_COMPRESSED_RGB:
+    case GL_COMPRESSED_SRGB:
+    case GL_COMPRESSED_RGB_BPTC_SIGNED_FLOAT:
+    case GL_COMPRESSED_RGB_BPTC_UNSIGNED_FLOAT:
+    case GL_COMPRESSED_RGB8_ETC2:
+    case GL_COMPRESSED_SRGB8_ETC2:
+      return GL_RGB;
+
+    case GL_RGBA:
+    case GL_RGBA2:
+    case GL_RGBA4:
+    case GL_RGB5_A1:
+    case GL_RGBA8:    case GL_RGBA8_SNORM:
+    case GL_RGBA8I:   case GL_RGBA8UI:
+    case GL_RGB10_A2: case GL_RGB10_A2UI:
+    case GL_RGBA12:
+    case GL_RGBA16:
+    case GL_RGBA16I:  case GL_RGBA16UI:
+    case GL_RGBA16F:
+    case GL_RGBA32I:  case GL_RGBA32UI:
+    case GL_RGBA32F:
+    case GL_SRGB_ALPHA:
+    case GL_SRGB8_ALPHA8:
+    case GL_COMPRESSED_RGBA:
+    case GL_COMPRESSED_SRGB_ALPHA:
+    case GL_COMPRESSED_RGBA_BPTC_UNORM:
+    case GL_COMPRESSED_SRGB_ALPHA_BPTC_UNORM:
+    case GL_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2:
+    case GL_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2:
+    case GL_COMPRESSED_RGBA8_ETC2_EAC:
+    case GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC:
+      return GL_RGBA;
+
+    default:
+      return GL_NONE;
+  }
 }
 
 
