842aa0de8f1a82995346f4f8ebc6b81a62c3afaf
Richard Bradley <hirdrac@users.noreply.github.com>
2022-05-16 03:23:16 +0900

    cleanup

--- gx/LIB_gx-staticGLFW.mk
+++ gx/LIB_gx-staticGLFW.mk
@@ -1,15 +1,8 @@
-# libgx.a build
+# libgx.a build w/ custom libglfw3.a
 
-LIB_gx = libgx
-LIB_gx.SRC =\
-  Logger.cc Unicode.cc System.cc Window.cc DrawContext.cc Font.cc Image.cc\
-  Texture.cc Camera.cc Gui.cc Renderer.cc OpenGLRenderer.cc OpenGL.cc\
-  3rd/glad.c 3rd/stb_image.c
-LIB_gx.PACKAGES = glfw3:3.2 freetype2
-LIB_gx.TYPE = static
-LIB_gx.STANDARD = c++17
+include LIB_gx.mk
 
-# settings if using custom libglfw.a
 LIB_gx.PACKAGES = freetype2
+  # freetype2:24 for SDF support
 LIB_gx.LIBS = dl pthread GLFW/libglfw3.a
 LIB_gx.INCLUDE = GLFW
--- gx/LIB_gx.mk
+++ gx/LIB_gx.mk
@@ -1,11 +1,12 @@
 # libgx.a build
 
 LIB_gx = libgx
-LIB_gx.SRC =\
-  Logger.cc Unicode.cc System.cc Window.cc DrawContext.cc Font.cc Image.cc\
-  Texture.cc Camera.cc Gui.cc Renderer.cc OpenGLRenderer.cc OpenGL.cc\
-  3rd/glad.c 3rd/stb_image.c
-LIB_gx.PACKAGES = glfw3:3.2 freetype2
-# freetype2:24 for SDF support
 LIB_gx.TYPE = static
 LIB_gx.STANDARD = c++17
+LIB_gx.SRC =\
+  Camera.cc DrawContext.cc Font.cc Gui.cc Image.cc Logger.cc OpenGL.cc\
+  OpenGLRenderer.cc Renderer.cc System.cc Texture.cc Unicode.cc Window.cc\
+  3rd/glad.c 3rd/stb_image.c
+
+LIB_gx.PACKAGES = glfw3:3.2 freetype2
+  # freetype2:24 for SDF support
