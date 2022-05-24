ab9dad5491d00dfc2b28eddb5f6dad704195b6f4
Richard Bradley <hirdrac@users.noreply.github.com>
2022-05-25 04:52:23 +0900

    disable asserts in stb_image if GX_NDEBUG is defined

--- gx/3rd/stb_image.c
+++ gx/3rd/stb_image.c
@@ -1,6 +1,10 @@
 // C file so implementation of stb_image is not compiled with C++ flags
 // also location of implementation specific defines (STBI_*)
 
+#ifdef GX_NDEBUG
+#define STBI_ASSERT(expr) ((void)(0))
+#endif
+
 #define STBI_NO_FAILURE_STRINGS
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
