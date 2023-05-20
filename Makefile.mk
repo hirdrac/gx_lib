91b9434318314470bc9da49f46c7bc39c3f3da60
Richard Bradley <hirdrac@users.noreply.github.com>
2023-05-20 22:30:08 +0900

    added DEFINE_TEST,INCLUDE_TEST settings

--- Makefile.mk
+++ Makefile.mk
@@ -1,5 +1,5 @@
 #
-# Makefile.mk - revision 50 (2023/5/17)
+# Makefile.mk - revision 50 (2023/5/20)
 # Copyright (C) 2023 Richard Bradley
 #
 # Additional contributions from:
@@ -88,9 +88,11 @@
 #  PACKAGES        list of packages for pkg-config
 #  PACKAGES_TEST   additional packages for all tests
 #  INCLUDE         includes needed not covered by pkg-config (-I optional)
+#  INCLUDE_TEST    additional includes for all tests (-I optional)
 #  LIBS            libraries needed not covered by pkg-config (-l optional)
 #  LIBS_TEST       additional libs for all tests (-l optional)
 #  DEFINE          defines for compilation (-D optional)
+#  DEFINE_TEST     additional defines for all tests (-D optional)
 #  OPTIONS         list of options to enable - use instead of specific flags
 #    warn_error    make all compiler warnings into errors
 #    pthread       compile with pthreads support
@@ -193,9 +195,11 @@ endif
 PACKAGES ?=
 PACKAGES_TEST ?=
 INCLUDE ?=
+INCLUDE_TEST ?=
 LIBS ?=
 LIBS_TEST ?=
 DEFINE ?=
+DEFINE_TEST ?=
 OPTIONS ?=
 FLAGS ?=
 FLAGS_TEST ?=
@@ -223,7 +227,7 @@ override BUILD_TMP := BUILD_TMP
 override LIBPREFIX := lib
 
 # apply *_EXTRA setting values (WARN_EXTRA handled above)
-$(foreach x,WARN_C WARN_CXX PACKAGES PACKAGES_TEST INCLUDE LIBS LIBS_TEST DEFINE OPTIONS FLAGS FLAGS_TEST FLAGS_RELEASE FLAGS_DEBUG FLAGS_PROFILE LINK_FLAGS EXCLUDE_TARGETS,\
+$(foreach x,WARN_C WARN_CXX PACKAGES PACKAGES_TEST INCLUDE INCLUDE_TEST LIBS LIBS_TEST DEFINE DEFINE_TEST OPTIONS FLAGS FLAGS_TEST FLAGS_RELEASE FLAGS_DEBUG FLAGS_PROFILE LINK_FLAGS EXCLUDE_TARGETS,\
   $(if $($x_EXTRA),$(eval override $x += $($x_EXTRA))))
 
 
@@ -712,7 +716,9 @@ else ifneq ($(_build_env),)
   override _pkgs_test := $(call _check_pkgs,PACKAGES_TEST)
 
   override _define := $(call _format_define,$(DEFINE))
+  override _define_test := $(call _format_define,$(DEFINE_TEST))
   override _include := $(call _format_include,$(INCLUDE))
+  override _include_test := $(call _format_include,$(INCLUDE_TEST))
   override _warn_cxx := $(call _format_warn,$(WARN_CXX))
   override _warn_c := $(call _format_warn,$(WARN_C))
 
@@ -727,9 +733,9 @@ else ifneq ($(_build_env),)
   ifneq ($(_test_labels),)
     override _test_pkg_flags := $(if $(_pkgs_test),$(call _get_pkg_flags,$(_pkgs) $(_pkgs_test)),$(_pkg_flags))
     override _test_xflags :=  $(_test_pkg_flags) $(FLAGS) $(FLAGS_$(_$(ENV)_uc)) $(FLAGS_TEST)
-    override _cxxflags_$(ENV)-tests := $(strip $(_cxx_std) $(_$(ENV)_opt) $(_warn_cxx) $(_op_cxx_warn) $(_define) $(_include) $(_op_cxx_flags) $(_test_xflags))
-    override _cflags_$(ENV)-tests := $(strip $(_c_std) $(_$(ENV)_opt) $(_warn_c) $(_op_warn) $(_define) $(_include) $(_op_flags) $(_test_xflags))
-    override _asflags_$(ENV)-tests := $(strip $(_$(ENV)_opt) $(_op_warn) $(_define) $(_include) $(_op_flags) $(_test_xflags))
+    override _cxxflags_$(ENV)-tests := $(strip $(_cxx_std) $(_$(ENV)_opt) $(_warn_cxx) $(_op_cxx_warn) $(_define) $(_define_test) $(_include) $(_include_test) $(_op_cxx_flags) $(_test_xflags))
+    override _cflags_$(ENV)-tests := $(strip $(_c_std) $(_$(ENV)_opt) $(_warn_c) $(_op_warn) $(_define) $(_define_test) $(_include) $(_include_test) $(_op_flags) $(_test_xflags))
+    override _asflags_$(ENV)-tests := $(strip $(_$(ENV)_opt) $(_op_warn) $(_define) $(_define_test) $(_include) $(_include_test) $(_op_flags) $(_test_xflags))
     override _src_path_$(ENV)-tests := $(_src_path)
   endif
 
@@ -839,11 +845,11 @@ else ifneq ($(_build_env),)
   endif
 
   ifneq ($$(strip $$($1.DEFINE)),-)
-    override _$1_define := $$(or $$(call _format_define,$$($1.DEFINE)),$$(_define))
+    override _$1_define := $$(or $$(call _format_define,$$($1.DEFINE)),$$(_define) $$(if $2,$$(_define_test)))
   endif
 
   ifneq ($$(strip $$($1.INCLUDE)),-)
-    override _$1_include := $$(or $$(call _format_include,$$($1.INCLUDE)),$$(_include))
+    override _$1_include := $$(or $$(call _format_include,$$($1.INCLUDE)),$$(_include) $$(if $2,$$(_include_test)))
   endif
 
   ifneq ($$(strip $$($1.FLAGS)),-)
--- tests/tests.mk
+++ tests/tests.mk
@@ -1,5 +1,8 @@
-FLAGS_TEST = -I. -UNDEBUG
+#FLAGS_TEST = -I. -UNDEBUG
+DEFINE_TEST = TEST -UNDEBUG
+INCLUDE_TEST = .
 
 TEST_MathUtil.SRC = tests/MathUtilTest.cc
 TEST_StringUtil.SRC = tests/StringUtilTest.cc
 TEST_Color.SRC = tests/ColorTest.cc
+TEST_FixedPoint.SRC = tests/FixedPointTest.cc
