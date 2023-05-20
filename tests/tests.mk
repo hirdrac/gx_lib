e8a948d23ac8a7840015729e3a16e49de72bd182
Richard Bradley <hirdrac@users.noreply.github.com>
2023-05-20 23:00:12 +0900

    removed unfinished test

--- tests/tests.mk
+++ tests/tests.mk
@@ -5,4 +5,3 @@ INCLUDE_TEST = .
 TEST_MathUtil.SRC = tests/MathUtilTest.cc
 TEST_StringUtil.SRC = tests/StringUtilTest.cc
 TEST_Color.SRC = tests/ColorTest.cc
-TEST_FixedPoint.SRC = tests/FixedPointTest.cc
