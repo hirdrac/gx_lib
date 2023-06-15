b5a3f70695b519ea062f15a1e2a02a1cb10b7231
Richard Bradley <hirdrac@users.noreply.github.com>
2023-06-15 15:07:21 +0900

    cleanup/rework

--- gx/Print.hh
+++ gx/Print.hh
@@ -1,8 +1,9 @@
 //
 // gx/Print.hh
-// Copyright (C) 2020 Richard Bradley
+// Copyright (C) 2023 Richard Bradley
 //
 // simplified stdout/stderr output
+// (note that these functions are not equivalent to C++23 print/println)
 //
 
 #pragma once
@@ -13,46 +14,42 @@ namespace gx
 {
   // print()
   template<typename... Args>
+  inline void print(std::ostream& os, const Args&... args)
+  {
+    ((os << args),...);
+  }
+
+  template<typename... Args>
   inline void print(const Args&... args)
   {
-    ((std::cout << args),...);
+    print(std::cout, args...);
   }
 
   // println()
   template<typename... Args>
-  inline void println(const Args&... args)
-  {
-    ((std::cout << args),...);
-    std::cout.put('\n');
-  }
-
-  // print_err()
-  template<typename... Args>
-  inline void print_err(const Args&... args)
-  {
-    ((std::cerr << args),...);
-  }
-
-  // println_err()
-  template<typename... Args>
-  inline void println_err(const Args&... args)
-  {
-    ((std::cerr << args),...);
-    std::cerr.put('\n');
-  }
-
-  // print_ostream()
-  template<typename... Args>
-  inline void print_ostream(std::ostream& os, const Args&... args)
-  {
-    ((os << args),...);
-  }
-
-  // println_ostream()
-  template<typename... Args>
-  inline void println_ostream(std::ostream& os, const Args&... args)
+  inline void println(std::ostream& os, const Args&... args)
   {
     ((os << args),...);
     os.put('\n');
+  }
+
+  template<typename... Args>
+  inline void println(const Args&... args)
+  {
+    println(std::cout, args...);
+  }
+
+
+  // std error output
+  template<typename... Args>
+  inline void print_err(const Args&... args)
+  {
+    print(std::cerr, args...);
+  }
+
+  template<typename... Args>
+  inline void println_err(const Args&... args)
+  {
+    println(std::cerr, args...);
   }
 }
