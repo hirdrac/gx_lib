e7120523aae791c8a2528964d8b8274670f5488f
Richard Bradley <hirdrac@users.noreply.github.com>
2023-11-24 12:37:41 +0900

    added explicit clear command

--- gx/OpenGLRenderer.cc
+++ gx/OpenGLRenderer.cc
@@ -120,6 +120,7 @@ namespace {
         case CMD_normal:       d += 2; break;
         case CMD_light:        d += 6; break;
         case CMD_no_light:     d += 1; break;
+        case CMD_clear:        d += 1; break;
         case CMD_line2:        d += 5;  vsize += 2; break;
         case CMD_line3:        d += 7;  vsize += 2; break;
         case CMD_line2C:       d += 7;  vsize += 2; break;
@@ -720,6 +721,10 @@ void OpenGLRenderer<VER>::draw(std::initializer_list<const DrawLayer*> dl)
 
         case CMD_lineWidth:
           addOp(OP_lineWidth, fval(d));
+          break;
+
+        case CMD_clear:
+          addOp(OP_clear, uint32_t(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
           break;
 
         case CMD_line2: {
