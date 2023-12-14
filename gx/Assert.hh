76cede72615332ca42177dfac5c0876fb5277312
Richard Bradley <hirdrac@users.noreply.github.com>
2023-12-14 15:18:41 +0900

    added GX_ASSERT_DEBUG() macro

--- gx/Assert.hh
+++ gx/Assert.hh
@@ -1,13 +1,28 @@
 //
 // gx/Assert.hh
-// Copyright (C) 2022 Richard Bradley
+// Copyright (C) 2023 Richard Bradley
 //
+
+// Macros (both disabled with NDEBUG or GX_NDEBUG)
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// GX_ASSERT()
+//   same as assert() but can be disabled without disabling regular assert()
+// GX_ASSERT_DEBUG()
+//   same as assert() but disabled by default, enable with GX_DEBUG_ASSERT
+//   (use for expensive asserts only needed for extreme debugging)
+
 
 #pragma once
 #include <cassert>
 
 #ifdef GX_NDEBUG
-#define GX_ASSERT(expr) static_cast<void>(0)
+#  define GX_ASSERT(expr) static_cast<void>(0)
+#  define GX_ASSERT_DEBUG(expr) static_cast<void>(0)
 #else
-#define GX_ASSERT(expr) assert(expr)
+#  define GX_ASSERT(expr) assert(expr)
+#  ifdef GX_DEBUG_ASSERT
+#    define GX_ASSERT_DEBUG(expr) assert(expr)
+#  else
+#    define GX_ASSERT_DEBUG(expr) static_cast<void>(0)
+#  endif
 #endif
