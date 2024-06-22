2c9b40fb54b434e32d9fb5ec704ad4c7df79ed53
Richard Bradley <hirdrac@users.noreply.github.com>
2024-06-22 20:03:09 +0900

    moved TextFormat to separate file

--- gx/DrawContext.hh
+++ gx/DrawContext.hh
@@ -16,16 +16,15 @@
 #include "Color.hh"
 #include "Normal.hh"
 #include "Align.hh"
+#include "TextFormat.hh"
 #include "Types.hh"
 #include <string_view>
 #include <initializer_list>
 
 
 namespace gx {
-  class Font;
   struct Glyph;
   class DrawContext;
-  struct TextFormat;
 
   struct Vertex2C {
     float x, y; uint32_t c;
@@ -46,30 +45,6 @@ namespace gx {
 
   struct Vertex3TCN { float x,y,z,s,t; uint32_t c, n; };
 }
-
-struct gx::TextFormat
-{
-  const Font* font = nullptr;
-  float lineSpacing = 0;  // extra spacing between lines
-  float glyphSpacing = 0; // extra spacing between glyphs
-  float tabWidth = 0;     // pixel width of tabs
-  Vec2 advX{1,0};         // dir of next glyph
-  Vec2 advY{0,1};         // dir of next line
-  Vec2 glyphX{1,0};       // glyph quad sides
-  Vec2 glyphY{0,1};
-
-  // transforms
-  void scaleX(float s) { advX *= s; glyphX *= s; }
-  void scaleY(float s) { advY *= s; glyphY *= s; }
-  void scale(float s) { scaleX(s); scaleY(s); }
-
-  void rotate(float rad) {
-    advX = gx::rotate(advX, rad);
-    advY = gx::rotate(advY, rad);
-    glyphX = gx::rotate(glyphX, rad);
-    glyphY = gx::rotate(glyphY, rad);
-  }
-};
 
 class gx::DrawContext
 {
---
+++ gx/TextFormat.hh
@@ -0,0 +1,37 @@
+//
+// gx/TextFormat.hh
+// Copyright (C) 2024 Richard Bradley
+//
+
+#pragma once
+#include "Types.hh"
+
+
+namespace gx {
+  class Font;
+  struct TextFormat;
+}
+
+struct gx::TextFormat
+{
+  const Font* font = nullptr;
+  float lineSpacing = 0;  // extra spacing between lines
+  float glyphSpacing = 0; // extra spacing between glyphs
+  float tabWidth = 0;     // pixel width of tabs
+  Vec2 advX{1,0};         // dir of next glyph
+  Vec2 advY{0,1};         // dir of next line
+  Vec2 glyphX{1,0};       // glyph quad sides
+  Vec2 glyphY{0,1};
+
+  // transforms
+  void scaleX(float s) { advX *= s; glyphX *= s; }
+  void scaleY(float s) { advY *= s; glyphY *= s; }
+  void scale(float s) { scaleX(s); scaleY(s); }
+
+  void rotate(float rad) {
+    advX = gx::rotate(advX, rad);
+    advY = gx::rotate(advY, rad);
+    glyphX = gx::rotate(glyphX, rad);
+    glyphY = gx::rotate(glyphY, rad);
+  }
+};
