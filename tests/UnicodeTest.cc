0341ac0c42bdebf11d5dabe0e7b568eba9489101
Richard Bradley <hirdrac@users.noreply.github.com>
2024-12-27 14:31:38 +0900

    added simple check for decoding invalid bytes in UTF8 string

--- tests/UnicodeTest.cc
+++ tests/UnicodeTest.cc
@@ -4,8 +4,25 @@
 //
 
 #include "gx/Unicode.hh"
+#include <cassert>
+using namespace gx;
+
 
 int main(int argc, char** argv)
 {
+  const unsigned char invalid_bytes[] = {
+    0xc0, 0xc1, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff
+  };
+
+  for (int ch : invalid_bytes) {
+    const char str[] = {char(ch)};
+    UTF8Iterator itr{str, 1};
+
+    assert(itr.get() == -1);
+    assert(!itr.done());
+    assert(!itr.next());
+    assert(itr.get() == -1);
+    assert(itr.done());
+  }
   return 0;
 }
