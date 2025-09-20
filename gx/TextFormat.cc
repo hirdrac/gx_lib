e12acca82ce1b561ea950a4371b631ec356a5811
Richard Bradley <hirdrac@users.noreply.github.com>
2025-09-20 16:06:32 +0900

    text color parsing improvements

--- gx/TextFormat.cc
+++ gx/TextFormat.cc
@@ -103,23 +103,47 @@ std::string_view TextFormat::fitText(
   return text;
 }
 
+[[nodiscard]] static constexpr int hexDigitVal(int val)
+{
+  if (val >= '0' && val <= '9') { return val - '0'; }
+  if (val >= 'A' && val <= 'F') { return (val - 'A') + 10; }
+  if (val >= 'a' && val <= 'f') { return (val - 'a') + 10; }
+  return -1;
+}
+
 [[nodiscard]] static std::optional<RGBA8> parseColorStr(std::string_view str)
 {
-  // TODO: add parsing hex color value
+  if (str.empty()) { return std::nullopt; }
 
-  switch (hashStr(str)) {
-    case hashStr("white"):   return packRGBA8(WHITE);
-    case hashStr("black"):   return packRGBA8(BLACK);
-    case hashStr("gray25"):  return packRGBA8(GRAY25);
-    case hashStr("gray50"):  return packRGBA8(GRAY50);
-    case hashStr("gray75"):  return packRGBA8(GRAY75);
-    case hashStr("red"):     return packRGBA8(RED);
-    case hashStr("green"):   return packRGBA8(GREEN);
-    case hashStr("blue"):    return packRGBA8(BLUE);
-    case hashStr("cyan"):    return packRGBA8(CYAN);
-    case hashStr("yellow"):  return packRGBA8(YELLOW);
-    case hashStr("magenta"): return packRGBA8(MAGENTA);
-    default:                 return std::nullopt;
+  if (str[0] == '#') {
+    if (str.size() != 7 && str.size() != 9) { return std::nullopt; }
+
+    int vals[8]{0,0,0,0,0,0,15,15};
+    const int num = int(str.size() - 1);
+    for (int i = 0; i < num; ++i) {
+      vals[i] = hexDigitVal(str[i+1]);
+      if (vals[i] < 0) { return std::nullopt; }
+    }
+
+    return packRGBA8i(uint8_t((vals[0]<<4) + vals[1]),
+                      uint8_t((vals[2]<<4) + vals[3]),
+                      uint8_t((vals[4]<<5) + vals[5]),
+                      uint8_t((vals[6]<<4) + vals[7]));
+  }
+
+  switch (hashStrLC(str)) {
+    case hashStrLC("white"):   return packRGBA8(WHITE);
+    case hashStrLC("black"):   return packRGBA8(BLACK);
+    case hashStrLC("gray25"):  return packRGBA8(GRAY25);
+    case hashStrLC("gray50"):  return packRGBA8(GRAY50);
+    case hashStrLC("gray75"):  return packRGBA8(GRAY75);
+    case hashStrLC("red"):     return packRGBA8(RED);
+    case hashStrLC("green"):   return packRGBA8(GREEN);
+    case hashStrLC("blue"):    return packRGBA8(BLUE);
+    case hashStrLC("cyan"):    return packRGBA8(CYAN);
+    case hashStrLC("yellow"):  return packRGBA8(YELLOW);
+    case hashStrLC("magenta"): return packRGBA8(MAGENTA);
+    default:                   return std::nullopt;
   }
 }
 
@@ -127,7 +151,7 @@ gx::TextMetaTagType TextFormat::parseTag(
   TextState& ts, std::string_view tag) const
 {
   if (tag.substr(0, 6) == "color=") {
-    const auto color = parseColorStr(tag.substr(6));
+    const auto color = parseColorStr(trimSpaces(tag.substr(6)));
     if (color.has_value()) {
       ts.pushColor(color.value());
       return TAG_color;
