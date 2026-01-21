e8a8eea3a71db9619bdb92db90a4ca1b708024f3
Richard Bradley <hirdrac@users.noreply.github.com>
2026-01-21 13:00:51 +0900

    added initial GuiBuilder test

---
+++ tests/GuiBuilderTest.cc
@@ -0,0 +1,17 @@
+//
+// GuiBuilderTest.cc
+// Copyright (C) 2026 Richard Bradley
+//
+
+#include "gx/GuiBuilder.hh"
+
+#ifdef NDEBUG
+#error "can't run test with NDEBUG"
+#endif
+
+
+int main(int argc, char** argv)
+{
+  // only checking that GuiBuilder.hh compiles without errors currently
+  return 0;
+}
