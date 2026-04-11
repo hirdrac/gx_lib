a29a85e0d7fedf7953ace557b5ff42fba452de8e
Richard Bradley <hirdrac@users.noreply.github.com>
2026-04-12 05:57:31 +0900

    rework InputEnum

--- gx/EventState.hh
+++ gx/EventState.hh
@@ -38,25 +38,29 @@ namespace gx {
     MODIFIER_SUPER = 1<<3,
   };
 
-  enum InputEnum {
-    // key values (adapted from glfw3.h)
+  enum InputEnum : int16_t {
+    // key values
     KEY_UNKNOWN = -1,
 
-    KEY_SPACE = 32, KEY_APOSTROPHE = 39, KEY_COMMA = 44,
-    KEY_MINUS = 45, KEY_PERIOD = 46, KEY_FWD_SLASH = 47,
-    KEY_0 = 48, KEY_1 = 49, KEY_2 = 50, KEY_3 = 51, KEY_4 = 52,
-    KEY_5 = 53, KEY_6 = 54, KEY_7 = 55, KEY_8 = 56, KEY_9 = 57,
-    KEY_SEMICOLON = 59, KEY_EQUAL = 61,
-    KEY_A = 65, KEY_B = 66, KEY_C = 67, KEY_D = 68, KEY_E = 69, KEY_F = 70,
-    KEY_G = 71, KEY_H = 72, KEY_I = 73, KEY_J = 74, KEY_K = 75, KEY_L = 76,
-    KEY_M = 77, KEY_N = 78, KEY_O = 79, KEY_P = 80, KEY_Q = 81, KEY_R = 82,
-    KEY_S = 83, KEY_T = 84, KEY_U = 85, KEY_V = 86, KEY_W = 87, KEY_X = 88,
-    KEY_Y = 89, KEY_Z = 90,
-    KEY_LBRACKET = 91, KEY_BACK_SLASH = 92, KEY_RBRACKET = 93, KEY_TILDA = 96,
+    // printable keys
+    KEY_SPACE = ' ', KEY_APOSTROPHE = '\'', KEY_COMMA = ',',
+    KEY_MINUS = '-', KEY_PERIOD = '.', KEY_SLASH = '/',
+    KEY_0 = '0', KEY_1 = '1', KEY_2 = '2', KEY_3 = '3', KEY_4 = '4',
+    KEY_5 = '5', KEY_6 = '6', KEY_7 = '7', KEY_8 = '8', KEY_9 = '9',
+    KEY_SEMICOLON = ';', KEY_EQUAL = '=',
+    KEY_A = 'A', KEY_B = 'B', KEY_C = 'C', KEY_D = 'D', KEY_E = 'E',
+    KEY_F = 'F', KEY_G = 'G', KEY_H = 'H', KEY_I = 'I', KEY_J = 'J',
+    KEY_K = 'K', KEY_L = 'L', KEY_M = 'M',
+    KEY_N = 'N', KEY_O = 'O', KEY_P = 'P', KEY_Q = 'Q', KEY_R = 'R',
+    KEY_S = 'S', KEY_T = 'T', KEY_U = 'U', KEY_V = 'U', KEY_W = 'W',
+    KEY_X = 'X', KEY_Y = 'Y', KEY_Z = 'Z',
+    KEY_LBRACKET = '[', KEY_BACKSLASH = '\\', KEY_RBRACKET = ']',
+    KEY_BACKTICK = '`',
 
-    KEY_WORLD_1 = 161, KEY_WORLD_2 = 162,
-    KEY_ESCAPE = 256, KEY_ENTER = 257, KEY_TAB = 258,
-    KEY_BACKSPACE = 259, KEY_INSERT = 260, KEY_DELETE = 261,
+    // function keys
+    KEY_ESCAPE = '\x1B', KEY_ENTER = '\r',
+    KEY_TAB = '\t', KEY_BACKSPACE = '\b',
+    KEY_INSERT = 260, KEY_DELETE = 261,
     KEY_RIGHT = 262, KEY_LEFT = 263, KEY_DOWN = 264, KEY_UP = 265,
     KEY_PAGE_UP = 266, KEY_PAGE_DOWN = 267, KEY_HOME = 268, KEY_END = 269,
     KEY_CAPS_LOCK = 280, KEY_SCROLL_LOCK = 281, KEY_NUM_LOCK = 282,
@@ -66,16 +70,18 @@ namespace gx {
     KEY_F11 = 300, KEY_F12 = 301, KEY_F13 = 302, KEY_F14 = 303, KEY_F15 = 304,
     KEY_F16 = 305, KEY_F17 = 306, KEY_F18 = 307, KEY_F19 = 308, KEY_F20 = 309,
     KEY_F21 = 310, KEY_F22 = 311, KEY_F23 = 312, KEY_F24 = 313, KEY_F25 = 314,
+    KEY_LSHIFT = 340, KEY_LCONTROL = 341,
+    KEY_LALT = 342, KEY_LSUPER = 343,
+    KEY_RSHIFT = 344, KEY_RCONTROL = 345,
+    KEY_RALT = 346, KEY_RSUPER = 347, KEY_MENU = 348,
+
+    // keypad keys
     KEY_KP_0 = 320, KEY_KP_1 = 321, KEY_KP_2 = 322, KEY_KP_3 = 323,
     KEY_KP_4 = 324, KEY_KP_5 = 325, KEY_KP_6 = 326, KEY_KP_7 = 327,
     KEY_KP_8 = 328, KEY_KP_9 = 329,
     KEY_KP_DECIMAL = 330, KEY_KP_DIVIDE = 331, KEY_KP_MULTIPLY = 332,
     KEY_KP_SUBTRACT = 333, KEY_KP_ADD = 334,
     KEY_KP_ENTER = 335, KEY_KP_EQUAL = 336,
-    KEY_LSHIFT = 340, KEY_LCONTROL = 341,
-    KEY_LALT = 342, KEY_LSUPER = 343,
-    KEY_RSHIFT = 344, KEY_RCONTROL = 345,
-    KEY_RALT = 346, KEY_RSUPER = 347, KEY_MENU = 348,
 
     // mouse buttons
     BUTTON_1 = 1001,  // left mouse button
@@ -97,7 +103,7 @@ namespace gx {
   }
 
   struct InputState {
-    int16_t value;       // InputEnum value
+    InputEnum value;     // InputEnum value
     int16_t scancode;    // platform-specific key value
     int8_t pressCount;   // number of press events since pollEvents
     int8_t repeatCount;  // number of repeat events since pollEvents
--- gx/Window.cc
+++ gx/Window.cc
@@ -581,6 +581,137 @@ static void sizeCB(GLFWwindow* win, int width, int height)
   if (impl->_renderer) { impl->_renderer->setFramebufferSize(width, height); }
 }
 
+static constexpr InputEnum translateGLFWKey(int key)
+{
+  switch (key) {
+    default: return KEY_UNKNOWN;
+
+    // printable keys
+    case GLFW_KEY_SPACE:         return KEY_SPACE;
+    case GLFW_KEY_APOSTROPHE:    return KEY_APOSTROPHE;
+    case GLFW_KEY_COMMA:         return KEY_COMMA;
+    case GLFW_KEY_MINUS:         return KEY_MINUS;
+    case GLFW_KEY_PERIOD:        return KEY_PERIOD;
+    case GLFW_KEY_SLASH:         return KEY_SLASH;
+    case GLFW_KEY_0:             return KEY_0;
+    case GLFW_KEY_1:             return KEY_1;
+    case GLFW_KEY_2:             return KEY_2;
+    case GLFW_KEY_3:             return KEY_3;
+    case GLFW_KEY_4:             return KEY_4;
+    case GLFW_KEY_5:             return KEY_5;
+    case GLFW_KEY_6:             return KEY_6;
+    case GLFW_KEY_7:             return KEY_7;
+    case GLFW_KEY_8:             return KEY_8;
+    case GLFW_KEY_9:             return KEY_9;
+    case GLFW_KEY_SEMICOLON:     return KEY_SEMICOLON;
+    case GLFW_KEY_EQUAL:         return KEY_EQUAL;
+    case GLFW_KEY_A:             return KEY_A;
+    case GLFW_KEY_B:             return KEY_B;
+    case GLFW_KEY_C:             return KEY_C;
+    case GLFW_KEY_D:             return KEY_D;
+    case GLFW_KEY_E:             return KEY_E;
+    case GLFW_KEY_F:             return KEY_F;
+    case GLFW_KEY_G:             return KEY_G;
+    case GLFW_KEY_H:             return KEY_H;
+    case GLFW_KEY_I:             return KEY_I;
+    case GLFW_KEY_J:             return KEY_J;
+    case GLFW_KEY_K:             return KEY_K;
+    case GLFW_KEY_L:             return KEY_L;
+    case GLFW_KEY_M:             return KEY_M;
+    case GLFW_KEY_N:             return KEY_N;
+    case GLFW_KEY_O:             return KEY_O;
+    case GLFW_KEY_P:             return KEY_P;
+    case GLFW_KEY_Q:             return KEY_Q;
+    case GLFW_KEY_R:             return KEY_R;
+    case GLFW_KEY_S:             return KEY_S;
+    case GLFW_KEY_T:             return KEY_T;
+    case GLFW_KEY_U:             return KEY_U;
+    case GLFW_KEY_V:             return KEY_V;
+    case GLFW_KEY_W:             return KEY_W;
+    case GLFW_KEY_X:             return KEY_X;
+    case GLFW_KEY_Y:             return KEY_Y;
+    case GLFW_KEY_Z:             return KEY_Z;
+    case GLFW_KEY_LEFT_BRACKET:  return KEY_LBRACKET;
+    case GLFW_KEY_BACKSLASH:     return KEY_BACKSLASH;
+    case GLFW_KEY_RIGHT_BRACKET: return KEY_RBRACKET;
+    case GLFW_KEY_GRAVE_ACCENT:  return KEY_BACKTICK;
+
+    // function keys
+    case GLFW_KEY_ESCAPE:        return KEY_ESCAPE;
+    case GLFW_KEY_ENTER:         return KEY_ENTER;
+    case GLFW_KEY_TAB:           return KEY_TAB;
+    case GLFW_KEY_BACKSPACE:     return KEY_BACKSPACE;
+    case GLFW_KEY_INSERT:        return KEY_INSERT;
+    case GLFW_KEY_DELETE:        return KEY_DELETE;
+    case GLFW_KEY_RIGHT:         return KEY_RIGHT;
+    case GLFW_KEY_LEFT:          return KEY_LEFT;
+    case GLFW_KEY_DOWN:          return KEY_DOWN;
+    case GLFW_KEY_UP:            return KEY_UP;
+    case GLFW_KEY_PAGE_UP:       return KEY_PAGE_UP;
+    case GLFW_KEY_PAGE_DOWN:     return KEY_PAGE_DOWN;
+    case GLFW_KEY_HOME:          return KEY_HOME;
+    case GLFW_KEY_END:           return KEY_END;
+    case GLFW_KEY_CAPS_LOCK:     return KEY_CAPS_LOCK;
+    case GLFW_KEY_SCROLL_LOCK:   return KEY_SCROLL_LOCK;
+    case GLFW_KEY_NUM_LOCK:      return KEY_NUM_LOCK;
+    case GLFW_KEY_PRINT_SCREEN:  return KEY_PRINT_SCREEN;
+    case GLFW_KEY_PAUSE:         return KEY_PAUSE;
+    case GLFW_KEY_F1:            return KEY_F1;
+    case GLFW_KEY_F2:            return KEY_F2;
+    case GLFW_KEY_F3:            return KEY_F3;
+    case GLFW_KEY_F4:            return KEY_F4;
+    case GLFW_KEY_F5:            return KEY_F5;
+    case GLFW_KEY_F6:            return KEY_F6;
+    case GLFW_KEY_F7:            return KEY_F7;
+    case GLFW_KEY_F8:            return KEY_F8;
+    case GLFW_KEY_F9:            return KEY_F9;
+    case GLFW_KEY_F10:           return KEY_F10;
+    case GLFW_KEY_F11:           return KEY_F11;
+    case GLFW_KEY_F12:           return KEY_F12;
+    case GLFW_KEY_F13:           return KEY_F13;
+    case GLFW_KEY_F14:           return KEY_F14;
+    case GLFW_KEY_F15:           return KEY_F15;
+    case GLFW_KEY_F16:           return KEY_F16;
+    case GLFW_KEY_F17:           return KEY_F17;
+    case GLFW_KEY_F18:           return KEY_F18;
+    case GLFW_KEY_F19:           return KEY_F19;
+    case GLFW_KEY_F20:           return KEY_F20;
+    case GLFW_KEY_F21:           return KEY_F21;
+    case GLFW_KEY_F22:           return KEY_F22;
+    case GLFW_KEY_F23:           return KEY_F23;
+    case GLFW_KEY_F24:           return KEY_F24;
+    case GLFW_KEY_F25:           return KEY_F25;
+    case GLFW_KEY_LEFT_SHIFT:    return KEY_LSHIFT;
+    case GLFW_KEY_LEFT_CONTROL:  return KEY_LCONTROL;
+    case GLFW_KEY_LEFT_ALT:      return KEY_LALT;
+    case GLFW_KEY_LEFT_SUPER:    return KEY_LSUPER;
+    case GLFW_KEY_RIGHT_SHIFT:   return KEY_RSHIFT;
+    case GLFW_KEY_RIGHT_CONTROL: return KEY_RCONTROL;
+    case GLFW_KEY_RIGHT_ALT:     return KEY_RALT;
+    case GLFW_KEY_RIGHT_SUPER:   return KEY_RSUPER;
+    case GLFW_KEY_MENU:          return KEY_MENU;
+
+    // keypad
+    case GLFW_KEY_KP_0:          return KEY_KP_0;
+    case GLFW_KEY_KP_1:          return KEY_KP_1;
+    case GLFW_KEY_KP_2:          return KEY_KP_2;
+    case GLFW_KEY_KP_3:          return KEY_KP_3;
+    case GLFW_KEY_KP_4:          return KEY_KP_4;
+    case GLFW_KEY_KP_5:          return KEY_KP_5;
+    case GLFW_KEY_KP_6:          return KEY_KP_6;
+    case GLFW_KEY_KP_7:          return KEY_KP_7;
+    case GLFW_KEY_KP_8:          return KEY_KP_8;
+    case GLFW_KEY_KP_9:          return KEY_KP_9;
+    case GLFW_KEY_KP_DECIMAL:    return KEY_KP_DECIMAL;
+    case GLFW_KEY_KP_DIVIDE:     return KEY_KP_DIVIDE;
+    case GLFW_KEY_KP_MULTIPLY:   return KEY_KP_MULTIPLY;
+    case GLFW_KEY_KP_SUBTRACT:   return KEY_KP_SUBTRACT;
+    case GLFW_KEY_KP_ADD:        return KEY_KP_ADD;
+    case GLFW_KEY_KP_ENTER:      return KEY_KP_ENTER;
+    case GLFW_KEY_KP_EQUAL:      return KEY_KP_EQUAL;
+  }  
+}
+
 static void keyCB(GLFWwindow* win, int key, int scancode, int action, int mods)
 {
   void* uPtr = glfwGetWindowUserPointer(win);
@@ -593,19 +724,21 @@ static void keyCB(GLFWwindow* win, int key, int scancode, int action, int mods)
   EventState& es = static_cast<WindowImpl*>(uPtr)->_eventState;
   es.events |= EVENT_KEY;
 
+  InputEnum val = translateGLFWKey(key);
   auto& states = es.keyStates;
   auto itr = std::find_if(states.begin(), states.end(),
-			  [key](auto& in){ return in.value == key; });
+			  [val](auto& in){ return in.value == val; });
   if (itr == states.end()) {
     itr = states.insert(
-      states.end(), {int16_t(key),int16_t(scancode),0,0,false});
+      states.end(), {val,int16_t(scancode),0,0,false});
   }
 
   InputState& in = *itr;
   if (action == GLFW_PRESS) {
     ++in.pressCount;
     in.held = true;
-    switch (key) {
+    switch (val) {
+      default: break;
       case KEY_LSHIFT:   case KEY_RSHIFT:
         es.mods |= MODIFIER_SHIFT;   ++es.shiftCount; break;
       case KEY_LCONTROL: case KEY_RCONTROL:
@@ -617,7 +750,8 @@ static void keyCB(GLFWwindow* win, int key, int scancode, int action, int mods)
     }
   } else if (action == GLFW_RELEASE) {
     in.held = false;
-    switch (key) {
+    switch (val) {
+      default: break;
       case KEY_LSHIFT:   case KEY_RSHIFT:
         if (--es.shiftCount <= 0) {
           es.shiftCount = 0; es.mods &= ~MODIFIER_SHIFT; }
@@ -704,7 +838,7 @@ static void mouseButtonCB(GLFWwindow* win, int button, int action, int mods)
   auto itr = std::find_if(states.begin(), states.end(),
 			  [bVal](auto& in){ return in.value == bVal; });
   if (itr == states.end()) {
-    itr = states.insert(states.end(), {int16_t(bVal),0,0,0,false});
+    itr = states.insert(states.end(), {InputEnum(bVal),0,0,0,false});
   }
 
   InputState& in = *itr;
