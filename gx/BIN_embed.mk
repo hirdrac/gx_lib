3d4a4bf03b6754e578452b05528d557a12c30f8f
Richard Bradley <hirdrac@users.noreply.github.com>
2026-04-27 01:18:27 +0900

    upgrade to C++20; prevent LTO

--- gx/BIN_embed.mk
+++ gx/BIN_embed.mk
@@ -1,9 +1,11 @@
-# embed build
+# embed build for Makefile.mk
+# Copyright (C) 2026 Richard Bradley
 
 BIN_embed.SRC = embed.cc
-BIN_embed.STANDARD = c++17
+BIN_embed.STANDARD = c++20
 BIN_embed.LIBS = -
 BIN_embed.PACKAGES = -
 BIN_embed.FLAGS = -
 BIN_embed.RPATH = -
 BIN_embed.LINK_FLAGS = -
+BIN_embed.OPTIONS = modern_c++
