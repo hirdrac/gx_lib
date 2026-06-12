9643161787b0ac0ca664f335aef3c43c87adb430
Richard Bradley <hirdrac@users.noreply.github.com>
2026-06-12 20:19:54 +0900

    allow printing enum class underlying value without typecast

--- gx/Print.hh
+++ gx/Print.hh
@@ -1,6 +1,6 @@
 //
 // gx/Print.hh
-// Copyright (C) 2025 Richard Bradley
+// Copyright (C) 2026 Richard Bradley
 //
 // simplified stdout/stderr output
 // (note that these functions are not equivalent to C++23 print/println)
@@ -12,43 +12,51 @@
 
 namespace gx
 {
-  // print()
-  template<class... Args>
-  void print(std::ostream& os, Args&&... args)
-  {
-    (os << ... << args);
-  }
-
-  template<class... Args>
-  void print(Args&&... args)
-  {
-    print(std::cout, args...);
-  }
-
-  // println()
-  template<class... Args>
-  void println(std::ostream& os, Args&&... args)
-  {
-    (os << ... << args) << '\n';
-  }
-
-  template<class... Args>
-  void println(Args&&... args)
-  {
-    println(std::cout, args...);
+  // value printing
+  template<class T>
+  void printval(std::ostream& os, T&& val) {
+    using type = std::remove_reference_t<T>;
+    if constexpr (std::is_enum_v<type>) {
+      os << static_cast<std::underlying_type_t<type>>(val);
+    } else {
+      os << val;
+    }
   }
 
 
-  // std error output
+  // print to ostream
   template<class... Args>
-  void print_err(Args&&... args)
-  {
-    print(std::cerr, args...);
+  void print_os(std::ostream& os, Args&&... args) {
+    (printval(os, args),...);
   }
 
   template<class... Args>
-  void println_err(Args&&... args)
-  {
-    println(std::cerr, args...);
+  void println_os(std::ostream& os, Args&&... args) {
+    (printval(os, args),...);
+    os.put('\n');
+  }
+
+
+  // print to cout
+  template<class... Args>
+  void print(Args&&... args) {
+    print_os(std::cout, args...);
+  }
+
+  template<class... Args>
+  void println(Args&&... args) {
+    println_os(std::cout, args...);
+  }
+
+
+  // print to cerr
+  template<class... Args>
+  void print_err(Args&&... args) {
+    print_os(std::cerr, args...);
+  }
+
+  template<class... Args>
+  void println_err(Args&&... args) {
+    println_os(std::cerr, args...);
   }
 }
